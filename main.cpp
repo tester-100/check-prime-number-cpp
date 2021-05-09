2c29909f6fd212e9f62c798f10946469941509ca
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-05-09 20:29:58 +0530

    Created main.cpp and added the source code of the program to it

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/**/
+
+#include <iostream>
+
+using namespace std;
+
+int checkPrime(int number) {
+	int factorsCount = 0;
+	for (int i = 1; i <= number; ++i) {
+		if ((number % i) == 0) {
+			factorsCount = factorsCount + 1;
+		}
+	}
+
+	if (factorsCount > 2) {
+		cout << "[ The entered number is a composite number ]" << endl;
+	} else {
+		cout << "[ The entered number is a prime number ]" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char const *argv[]) {
+	int number;
+	cout << "Enter the number : ";
+	cin >> number;
+	checkPrime(number);
+	return 0;
+}
